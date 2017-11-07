3a1036648ab4c3e33e61a6c625a4b9545eba84bd
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-11-08 07:24:48 +0800

    Membuat inputan dan menyimpan nilai

--- soal 4.cpp
+++ soal 4.cpp
@@ -8,6 +8,17 @@ int main(){
 	cout<<"Input Bilangan : ";cin>>x;
 	cout<<""<<endl;
 	if (x < 0){
+		cout<<"yang anda input adalah bilangan negatif"<<endl;
+	}
+	
+	else if (x>0)
+	{
+		cout<<"yang anda input adalah bilangan positif"<<endl;
+	}
+	else
+	
+		cout<<"yang anda input adalah bilangan nol "<<endl;
+
+	return 0;
 		
 	}
-}
