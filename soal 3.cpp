e462768e348ada7de35ce3e9f1f6aaff5896eab1
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-11-08 00:09:16 +0800

    Memberi inputan dan menyimpan nilai

--- soal 3.cpp
+++ soal 3.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
 	float jk, le, gtot, gp, gb,ul, jam,pa;
+	cout<<"   PROGRAM GAJI BERSIH KARYAWAN"<<endl;
+	cout<<""<<endl;
+	cout<<"Lama Kerja Karyawan    : "; cin>> jk;
+	cout<<"Gaji Perjam Karyawan   : "; cin>>gp;
 	
 }
