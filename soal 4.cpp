4a6aff05e3c159f7fd2e45336fefee8df4c03e42
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-11-08 07:21:02 +0800

    Menambahkan variabel

--- soal 4.cpp
+++ soal 4.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-	
+	int x;
+	cout<<"PROGRAM MENENTUKAN JENIS BILANGAN"<<endl;
+	cout<<""<<endl;
+	cout<<"Input Bilangan : ";cin>>x;
+	cout<<""<<endl;
+	if (x < 0){
+		
+	}
 }
