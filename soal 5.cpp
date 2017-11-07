4dea9b238e5253a5fed6923ed6303a2064832c66
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-11-08 07:33:31 +0800

    Membuat perhitungan dan menyelesaikan soal 5 (selesai)

--- soal 5.cpp
+++ soal 5.cpp
@@ -5,5 +5,18 @@ using namespace std;
 int main(){
 	float beratbadan;
     int tinggibadan;
+    
+     cout << "Silahkan masukkan tinggi badan(cm) : "; cin >> tinggibadan;
+    cout << "Silahkan masukkan berat badan(kg) : " ; cin >> beratbadan;
+    
+     if (beratbadan<tinggibadan/2.5)
+       cout << "  Anda Mengalami : Underweight(kekurangan berat badan)" << endl;
+            else if (tinggibadan/2.3 < beratbadan)
+                 cout << "  Anda Mengalami : Overweight(kelebihan berat badan)" << endl;
+            else if (tinggibadan/2.5 <= beratbadan <= tinggibadan/2.3)
+           		 cout << "  congratulation!!! : Normal" << endl;
+                 
+                 
+                 return 0;
 
 }
